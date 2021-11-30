5c7a3fce566f1c4191ea0feb9578654e79a05c2b
William Haugerud <74253011+WilliamHaugerud@users.noreply.github.com>
2021-11-30 14:24:50 +0100

    Update Bodeplotter.py

--- Bodeplotter.py
+++ Bodeplotter.py
@@ -22,17 +22,25 @@ while line !="":
     line = f.readline()
 
 
-
+absolute_difference_function = lambda list_value : abs(list_value - (max(gain)-3))
+closest_value = min(gain, key=absolute_difference_function)
+gain.index(closest_value)
+freq_comment = str(freq[gain.index(closest_value)])
 
 
 plt.figure()
 plt.subplot(211)
 plt.semilogx(freq, gain)
+#Comment out these lines to plot withou the -3 dB point
+plt.scatter(freq[gain.index(closest_value)], gain[gain.index(closest_value)], color = 'red')
+plt.annotate('-3dB at {}Hz'.format(freq_comment), (freq[gain.index(closest_value)], gain[gain.index(closest_value)]))
+
 plt.grid()
 plt.ylabel('dB Mag.')
 #plt.ylim([-120,5])
 plt.subplot(212)
 plt.semilogx(freq, phase)
+plt.grid()
 plt.xlabel('Freq. (Hz)')
 plt.ylabel('Phase (deg.)')
 #plt.ylim([-185,10])

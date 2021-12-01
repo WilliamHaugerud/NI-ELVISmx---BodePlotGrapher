6ec8711e9e336cd3c76669c46d17851e68b1644b
William Haugerud <74253011+WilliamHaugerud@users.noreply.github.com>
2021-12-01 13:04:35 +0100

    Update Bodeplotter.py

--- Bodeplotter.py
+++ Bodeplotter.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-f = open("op.txt", "r")
+f = open("oputenADC.txt", "r")
 line1 = f.readline()
 date, time = line1.split()
 line2 = f.readline()
@@ -21,28 +21,30 @@ while line !="":
     phase.append(float(phaseRaw.replace(',', '.')))
     line = f.readline()
 
-
+#Finding the -3 dB value and finding the value nearest this value in the list.
 absolute_difference_function = lambda list_value : abs(list_value - (max(gain)-3))
 closest_value = min(gain, key=absolute_difference_function)
-gain.index(closest_value)
+peak_dB_ELVIS = max(gain)
+x_coord_ELVIS = freq[gain.index(closest_value)]
+y_coord_ELVIS = gain[gain.index(closest_value)]
 freq_comment = str(freq[gain.index(closest_value)])
 
-
+#dB plot
 plt.figure()
 plt.subplot(211)
 plt.semilogx(freq, gain)
-#Comment out these lines to plot withou the -3 dB point
-plt.scatter(freq[gain.index(closest_value)], gain[gain.index(closest_value)], color = 'red')
-plt.annotate('-3dB at {}Hz'.format(freq_comment), (freq[gain.index(closest_value)], gain[gain.index(closest_value)]))
+#Comment out these lines to plot without the -3 dB point
+plt.scatter(x_coord_ELVIS, y_coord_ELVIS, color = 'red')
+plt.annotate('-3dB at {}Hz'.format(freq_comment), (x_coord_ELVIS, y_coord_ELVIS))
+plt.legend(['Peak value {}dB'.format(peak_dB_ELVIS)], loc ="lower left") #Use this to change the location
 
-plt.grid()
 plt.ylabel('dB Mag.')
-#plt.ylim([-120,5])
+plt.grid()
+#Phaseplot
 plt.subplot(212)
 plt.semilogx(freq, phase)
-plt.grid()
 plt.xlabel('Freq. (Hz)')
 plt.ylabel('Phase (deg.)')
-#plt.ylim([-185,10])
-#out = plt.yticks(np.arange(-180,5,45))
+plt.grid()
 plt.show()
+
